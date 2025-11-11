376017fbdc85162a40bc5af95af66747622b6b4e
Rubinnz <vuquockhanhdz@gmail.com>
2025-11-11 12:18:07 +0700

    del file

--- test.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-model_pkg = joblib.load("hybrid_model.pkl")
-recommender = model_pkg["hybrid_recommender"]
-
-# Get recommendations:
-recommendations = recommender.recommend(user_id="user_4971", top_n=10)
-for i in enumerate(recommendations, 1):
-    print(i)
